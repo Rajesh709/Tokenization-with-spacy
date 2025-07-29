7ee632f4e9dd2e55c0c3f055b24b9534625fcfe0
rajesh <m.rajesh@centrahub.com>
2025-07-29 11:47:28 +0530

    add file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import spacy
+
+# Tokenization
+
 nlp = spacy.load("en_core_web_sm")
+#nlp = spacy.load("en_core_web_sm")
 
 doc=nlp("spacy is workin on MAC!")
 
 for token in doc:
     print(token.text,token.pos_,token.dep_)
+
+#2. Lemmatization#
+for token in doc:
+        print(f"{token.text} → {token.lemma_}")
